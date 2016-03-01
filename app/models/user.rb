064041f1e80d96f2998349e640cf4cc73a11f488
Michal Buszkiewicz <embed1@gmail.com>
2016-03-01 23:32:37 +0100

    User#total_refunds added along with specs for the model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,12 @@ class User < ActiveRecord::Base
 
   validates :full_name, presence: true,
                         length: { minimum: 2, tokenizer: ->(str) { str.scan(/\w+/) } }
+
+  def total_refunds(year, month)
+    month_start = Date.new(year, month)
+    month_end = month_start.end_of_month
+    accepted_requests = refund_requests.accepted.where(created_at: month_start..month_end)
+    accepted_requests.collect { |r| r.refunded_amount }.sum
+  end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -32,4 +32,38 @@ RSpec.describe User, type: :model do
     it { is_expected.to have_many(:refund_requests) }
   end
 
+  describe 'total refunds for a month' do
+    let(:category1) { FactoryGirl.create :category, refund_percentage: 30 }
+    let(:category2) { FactoryGirl.create :category, refund_percentage: 70 }
+    let(:another_user) { FactoryGirl.build :user, email: user.email }
+    subject { user }
+
+    before do
+      # these should be counted:
+      FactoryGirl.create :refund_request, category: category1,
+                          status: :accepted, amount: 50, user: user,
+                          created_at: '1 May 2015'
+      FactoryGirl.create :refund_request, category: category2,
+                          status: :accepted, amount: 100, user: user,
+                          created_at: '10 May 2015'
+      # these shouldn't be counted (another month, not accepted, another user)
+      FactoryGirl.create :refund_request, category: category2,
+                          status: :accepted, amount: 150, user: user,
+                          created_at: '20 Jun 2015'
+      FactoryGirl.create :refund_request, category: category1,
+                          status: :pending, amount: 50, user: user,
+                          created_at: '10 May 2015'
+      FactoryGirl.create :refund_request, category: category1,
+                          status: :accepted, amount: 25, user: another_user,
+                          created_at: '10 May 2015'
+    end
+
+    it { is_expected.to respond_to(:total_refunds).with(2).arguments }
+
+    it 'should return the correct total for the user\'s refunds' do
+      expect(user.total_refunds(2015, 5)).to eql(85)
+    end
+
+  end
+
 end
