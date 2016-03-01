be8bbd6260c302956b10f65f0e0061e8875cbbf9
Michal Buszkiewicz <embed1@gmail.com>
2016-03-01 15:22:45 +0100

    Admin field added to user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160301133503) do
+ActiveRecord::Schema.define(version: 20160301142216) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "title"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 20160301133503) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "full_name"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
