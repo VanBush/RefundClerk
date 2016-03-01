a10d169236e3806a35ed12dc9897c9e97a71235f
Michal Buszkiewicz <embed1@gmail.com>
2016-03-01 17:56:43 +0100

    Tests and some validation alterations for category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,7 @@
 class Category < ActiveRecord::Base
   has_many :refund_requests
 
-  validates :title, presence: true
+  validates :title, presence: true, uniqueness: true
   validates :refund_percentage, numericality: { greater_than: 0,
                                                 less_than_or_equal_to: 100 }
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:category) { FactoryGirl.create :category }
+  subject { category }
+
+  describe 'with all valid values' do
+    it { is_expected.to be_valid }
+  end
+
+  describe 'without a title' do
+    before { category.title = '' }
+    it { is_expected.not_to be_valid }
+  end
+
+  describe 'with less than zero refund percentage' do
+    before { category.refund_percentage = -10 }
+    it { is_expected.not_to be_valid }
+  end
+
+  describe 'with zero refund percentage' do
+    before { category.refund_percentage = 0 }
+    it { is_expected.not_to be_valid }
+  end
+
+  describe 'with just above zero refund percentage' do
+    before { category.refund_percentage = 1 }
+    it { is_expected.to be_valid }
+  end
+
+  describe 'with maximum refund percentage' do
+    before { category.refund_percentage = 100 }
+    it { is_expected.to be_valid }
+  end
+
+  describe 'with just over maximum refund percentage' do
+    before { category.refund_percentage = 101 }
+    it { is_expected.not_to be_valid }
+  end
+
+  describe 'associations' do
+    it { is_expected.to have_many :refund_requests }
+  end
+
 end
