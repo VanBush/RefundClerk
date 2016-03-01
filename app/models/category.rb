3388129055d199b2272ae308b573335a95d888b7
Michal Buszkiewicz <embed1@gmail.com>
2016-03-01 10:32:53 +0100

    Validations for category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,7 @@
 class Category < ActiveRecord::Base
   has_many :refund_requests
+
+  validates :title, presence: true
+  validates :refund_percentage, numericality: { greater_than: 0,
+                                                less_than_or_equal_to: 100 }
 end
