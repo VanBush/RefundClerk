056c649fc1fd5abe67f9c853c112f5f959016cfe
Michal Buszkiewicz <embed1@gmail.com>
2016-03-01 14:15:11 +0100

    Factories for models

---
+++ spec/factories/categories.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :category do
+    title 'Test Category'
+    refund_percentage 50
+  end
+end
---
+++ spec/factories/refund_requests.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :refund_request do
+    title Faker::Commerce.product_name
+    description Faker::Lorem.paragraph
+    amount Faker::Number.decimal(3, 2)
+    approved false
+  end
+end
