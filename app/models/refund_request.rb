44034acd8cf04ba4041e9f6caddb39ce5c47b282
Michal Buszkiewicz <embed1@gmail.com>
2016-03-01 09:57:51 +0100

    Validations for refund request model

--- app/models/refund_request.rb
+++ app/models/refund_request.rb
@@ -1,4 +1,12 @@
 class RefundRequest < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
+
+  validates :user, presence: true
+  validates :category, presence: true
+
+  validates :description, presence: true
+  validates :title, presence: true, length: { maximum: 50 }
+
+  validates :amount, numericality: { greater_than: 0 }
 end
