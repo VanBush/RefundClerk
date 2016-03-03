ac359bc5ad43426456738c44f9831a8240347364
Michał Buszkiewicz <embed1@gmail.com>
2016-03-03 08:52:47 +0100

    Refund requests controller with specs

---
+++ app/controllers/refund_requests_controller.rb
@@ -0,0 +1,59 @@
+class RefundRequestsController < ApplicationController
+  def index
+    authorize RefundRequest
+    @refund_requests = policy_scope(RefundRequest)
+    if params[:user_id].present?
+      @refund_requests.select! { |r| r.user_id == params[:user_id] }
+    end
+  end
+
+  def show
+    @refund_request = RefundRequest.find(params[:id]).decorate
+    authorize @refund_request
+  end
+
+  def new
+    @refund_request = RefundRequest.new
+    authorize @refund_request
+    @refund_request.user = current_user
+  end
+
+  def create
+    @refund_request = RefundRequest.new(user: current_user)
+    authorize @refund_request
+    @refund_request.assign_attributes(permitted_attributes(@refund_request))
+    if @refund_request.save
+      redirect_to refund_request_path(@refund_request),
+                  notice: 'Refund request has been created'
+    else
+      render :new, flash: { error: 'Refund request could not be created' }
+    end
+  end
+
+  def edit
+    @refund_request = RefundRequest.find(params[:id]).decorate
+    authorize @refund_request
+  end
+
+  def update
+    @refund_request = RefundRequest.find(params[:id]).decorate
+    authorize @refund_request
+    if @refund_request.update_attributes(permitted_attributes(@refund_request))
+      render :show, notice: 'Refund request has been updated'
+    else
+      render :new, flash: { error: 'Refund request could not be updated' }
+    end
+  end
+
+  def destroy
+    @refund_request = RefundRequest.find(params[:id])
+    authorize @refund_request
+    if @refund_request.destroy
+      redirect_to refund_requests_path, notice: 'Refund request has been deleted'
+    else
+      redirect_to refund_request_path(@refund_request),
+                  flash: { error: 'Refund request could not be deleted' }
+    end
+  end
+
+end
--- spec/controllers/refund_requests_controller_spec.rb
+++ spec/controllers/refund_requests_controller_spec.rb
@@ -13,9 +13,10 @@ RSpec.describe RefundRequestsController, type: :controller do
 
   subject { response }
 
-  shared_examples 'successfully renders template' do |template|
+  shared_examples 'successfully renders template' do |method, action|
+    before { send method, action, request_params }
     it { is_expected.to have_http_status(200) }
-    it { is_expected.to render_template(template) }
+    it { is_expected.to render_template(action) }
   end
 
   shared_examples 'redirects to sessions#new if not logged in' do |method, action|
@@ -29,15 +30,13 @@ RSpec.describe RefundRequestsController, type: :controller do
     it_has_behavior 'redirects to sessions#new if not logged in', :get, :index
 
     context 'when logged in as an user' do
-      before { controller.stub(:current_user).and_return(users.first)
-               get :index }
-      it_has_behavior 'successfully renders template', :index
+      before { controller.stub(:current_user).and_return(users.first) }
+      it_has_behavior 'successfully renders template', :get, :index
     end
 
     context 'when logged in as admin' do
-      before { controller.stub(:current_user).and_return(admin)
-               get :index }
-      it_has_behavior 'successfully renders template', :index
+      before { controller.stub(:current_user).and_return(admin) }
+      it_has_behavior 'successfully renders template', :get, :index
     end
   end
 
@@ -47,9 +46,9 @@ RSpec.describe RefundRequestsController, type: :controller do
 
     context 'when logged in as an user' do
       context 'and accessing that user\'s item' do
-        before { controller.stub(:current_user).and_return(users.first)
-                 get :show, id: users.first.refund_requests.first }
-        it_has_behavior 'successfully renders template', :show
+        let(:request_params) { { id: users.first.refund_requests.first } }
+        before { controller.stub(:current_user).and_return(users.first) }
+        it_has_behavior 'successfully renders template', :get, :show
       end
 
       context 'and accessing another user\'s item' do
@@ -60,64 +59,85 @@ RSpec.describe RefundRequestsController, type: :controller do
     end
 
     context 'when logged in as admin' do
-      before { controller.stub(:current_user).and_return(admin)
-               get :show, id: users.second.refund_requests.first }
-      it_has_behavior 'successfully renders template', :show
+      let(:request_params) { { id: users.second.refund_requests.first } }
+      before { controller.stub(:current_user).and_return(admin) }
+      it_has_behavior 'successfully renders template', :get, :show
     end
   end
 
-  # describe 'GET #new' do
-  #   it_has_behavior 'redirects to sessions#new if not logged in', :get, :new
-  #
-  #   context 'when logged in' do
-  #     it 'should be ok'
-  #   end
-  # end
-  #
-  # describe 'POST #create' do
-  #   context 'when not logged in' do
-  #     it 'should redirect to sessions#new'
-  #   end
-  #
-  #   context 'when logged in' do
-  #     it 'should be ok'
-  #     it 'should create a record'
-  #   end
-  # end
-  #
-  # describe 'GET #edit' do
-  #   context 'when not logged in' do
-  #     it 'should redirect to sessions#new'
-  #   end
-  #
-  #   context 'when logged in' do
-  #     context 'as the item\'s owner' do
-  #       it 'should be ok'
-  #       it 'should show the item\'s title'
-  #     end
-  #
-  #     context 'as someone else' do
-  #       it 'should be unauthorized'
-  #     end
-  #   end
-  # end
-  #
-  # describe 'PATCH #update' do
-  #   context 'when not logged in' do
-  #     it 'should redirect to sessions#new'
-  #   end
-  #
-  #   context 'when logged in' do
-  #     context 'as the item\'s owner' do
-  #       it 'should be ok'
-  #       it 'should update the item'
-  #     end
-  #
-  #     context 'as someone else' do
-  #       it 'should be unauthorized'
-  #     end
-  #   end
-  # end
+  describe 'GET #new' do
+    it_has_behavior 'redirects to sessions#new if not logged in', :get, :new
+
+    context 'when logged in' do
+      before { controller.stub(:current_user).and_return(users.first) }
+      it_has_behavior 'successfully renders template', :get, :new
+    end
+  end
+
+  describe 'POST #create' do
+    let(:request_params) { { refund_request:
+                               FactoryGirl.attributes_for(:refund_request,
+                               category_id: category.id)
+                             } }
+    it_has_behavior 'redirects to sessions#new if not logged in', :post, :create
+
+    context 'when logged in' do
+      before { controller.stub(:current_user).and_return(users.first) }
+      it 'creates a record' do
+        expect { post :create, request_params }
+          .to change { users.first.refund_requests.count }.by(1)
+      end
+    end
+  end
+
+  describe 'GET #edit' do
+    let(:request_params) { { id: RefundRequest.first.id } }
+    it_has_behavior 'redirects to sessions#new if not logged in', :get, :edit
+
+    context 'when logged in as an user' do
+      context 'and accessing that user\'s item' do
+        let(:request_params) { { id: users.first.refund_requests.first } }
+        before { controller.stub(:current_user).and_return(users.first) }
+        it_has_behavior 'successfully renders template', :get, :edit
+      end
+
+      context 'and accessing another user\'s item' do
+        before { controller.stub(:current_user).and_return(users.first)
+          get :edit, id: users.second.refund_requests.first }
+        it { is_expected.to have_http_status(403) }
+      end
+
+      context 'when logged in as admin' do
+        let(:request_params) { { id: users.second.refund_requests.first } }
+        before { controller.stub(:current_user).and_return(admin) }
+        it_has_behavior 'successfully renders template', :get, :edit
+      end
+    end
+
+  end
+
+  describe 'PATCH #update' do
+    let(:request_params) { { id: users.first.refund_requests.first.id,
+                             refund_request: { title: 'blabla' } } }
+    it_has_behavior 'redirects to sessions#new if not logged in', :patch, :update
+
+    context 'when logged in' do
+      context 'as the item\'s owner' do
+        before { controller.stub(:current_user).and_return(users.first)
+                 patch :update, request_params }
+        it { is_expected.to have_http_status(200) }
+        it 'should update the item' do
+          expect(users.first.refund_requests.first.title).to eql('blabla')
+        end
+      end
+
+      context 'as someone else' do
+        before { controller.stub(:current_user).and_return(users.second)
+                 patch :update, request_params }
+        it { is_expected.to have_http_status(403) }
+      end
+    end
+  end
   #
   # describe 'DELETE #destroy' do
   #   context 'when not logged in' do
