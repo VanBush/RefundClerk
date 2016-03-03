6fd8b614fc183a4e4c8a461ede27ef5d8fa5aaf7
Michał Buszkiewicz <embed1@gmail.com>
2016-03-03 08:49:02 +0100

    Handling Pundit::NotAuthorizedError with redirecting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,20 @@
 class ApplicationController < ActionController::Base
   include Pundit
-  
+  after_action :verify_authorized
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+    def user_not_authorized(exception)
+      if user_signed_in?
+        redirect_to root_url, alert: exception.message, status: :forbidden
+      else
+        redirect_to new_user_session_url, alert: 'Please sign in.'
+      end
+    end
 end
