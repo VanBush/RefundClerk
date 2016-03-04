bc25dc4b0cc4987419f2a13dd0a243325c5dcf15
Michal Buszkiewicz <embed1@gmail.com>
2016-03-04 22:45:02 +0100

    cron changes and actual monthly report

--- app/mailers/report_mailer.rb
+++ app/mailers/report_mailer.rb
@@ -2,7 +2,9 @@ class ReportMailer < ApplicationMailer
   default from: 'vanbush@example.com'
 
   def report_email(month, year)
-    @users = User.all
+    @report_records = RefundRequest.monthly_summary(month, year)
+    @month = month
+    @year = year
     recipients = User.where(admin: true).pluck(:email)
     recipients.each do |recipient|
       mail(from: 'vanbush@example.com',
--- app/workers/report_mail_worker.rb
+++ app/workers/report_mail_worker.rb
@@ -1,9 +1,9 @@
 class ReportMailWorker
 	include Sidekiq::Worker
 	def perform
-		ReportMailer.report_email.deliver_now(
+		ReportMailer.report_email(
 			Date.current.prev_month.month,
 			Date.current.prev_month.year,
-		)
+		).deliver_now
 	end
 end
