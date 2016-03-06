db88890e54e566f346aa2c5c491cc42b291bd516
Michal Buszkiewicz <embed1@gmail.com>
2016-03-06 10:08:04 +0100

    some finally meaningful views

--- app/decorators/refund_request_decorator.rb
+++ app/decorators/refund_request_decorator.rb
@@ -1,13 +1,43 @@
 class RefundRequestDecorator < Draper::Decorator
   delegate_all
 
-  # Define presentation-specific methods here. Helpers are accessed through
-  # `helpers` (aka `h`). You can override attributes, for example:
-  #
-  #   def created_at
-  #     helpers.content_tag :span, class: 'time' do
-  #       object.created_at.strftime("%a %m/%d/%y")
-  #     end
-  #   end
+  def created_at
+    h.content_tag :span, class: 'time' do
+      object.created_at.strftime("%a %m-%d-%Y")
+    end
+  end
+
+  def delete_link
+    h.link_to 'Delete', object, method: :delete,
+              data: { confirm: 'Are you sure?' }
+  end
+
+  def details_link
+    h.link_to object.title, object
+  end
+
+  def status
+    h.content_tag :span, class: "status-#{object.status}" do
+      object.status
+    end
+  end
+
+  def category
+    text = object.category.title
+    return h.link_to text, object.category if h.current_user.admin?
+    text
+  end
+
+  def amount
+    h.number_to_currency(object.amount)
+  end
+
+  def refund
+    "#{h.number_to_currency(object.refunded_amount)} (#{object.category.refund_percentage}%)"
+  end
+
+  def user
+    h.link_to object.user.full_name, h.refund_requests_url(user: object.user)
+  end
 
 end
