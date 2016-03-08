3c11d62cc5defcc6ff00fabdb5fdb63ed0d9838b
Michal Buszkiewicz <embed1@gmail.com>
2016-03-08 01:25:17 +0100

    fixed category title

--- spec/factories/categories.rb
+++ spec/factories/categories.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :category do
-    sequence(:title) { |n| "Faker::Commerce.department #{n}" }
+    sequence(:title) { |n| "#{Faker::Commerce.department} #{n}" }
     refund_percentage 50
   end
 end
